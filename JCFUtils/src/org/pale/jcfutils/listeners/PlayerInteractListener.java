3a4c0e39bb508b9b9f836e42f16d98d4fe514f48
Jim Finnis (on Hecate) <jim.finnis@gmail.com>
2019-07-28 23:48:08 +0100

    more with the dowsy stick

--- JCFUtils/src/org/pale/jcfutils/listeners/PlayerInteractListener.java
+++ JCFUtils/src/org/pale/jcfutils/listeners/PlayerInteractListener.java
@@ -18,76 +18,79 @@ import org.bukkit.util.Vector;
 
 
 public class PlayerInteractListener implements Listener {
-	@EventHandler
-	public void onPlayerInteract(PlayerInteractEvent e) {
-		if(e.getAction()==Action.RIGHT_CLICK_BLOCK && e.getHand().equals(EquipmentSlot.HAND)) {
-			Player p = e.getPlayer();
-			ItemStack st = p.getInventory().getItemInMainHand();
-			ItemMeta meta = st.getItemMeta();
-			List<String> lore = meta.getLore();
-			if(lore != null && lore.size()>1) {
-				if(lore.get(0)=="Magic Stick"){
-					Material m = Material.getMaterial(lore.get(1));
-					scan(p,m);
-					e.setCancelled(true);
-				}
-			}
-		}
-	}	
-
-	static final int DIST_NEAR=10;
-	static final int DIST_MID=20;
-	static final int DIST_FAR=60;
-	static final double ANGLE=45 * (Math.PI/180.0);
-
-	private void scan(Player p,Material m) {
-		Location l = p.getLocation();
-		int x = l.getBlockX();
-		int y = l.getBlockY();
-		int z = l.getBlockZ();
-		World w = l.getWorld();
-
-		Vector lineOfSight = p.getEyeLocation().getDirection();
-
-		int ctnear =0;
-		int ctmid = 0;
-		int ctfar = 0;
-		int ctsight=0;
-		int ctsightnear=0;
-		
-		p.sendMessage("looking (insert mystical sounds here)");
-		for(int xoffset=-DIST_FAR;xoffset<=DIST_FAR;xoffset++) {
-			for(int yoffset=-DIST_FAR;yoffset<=DIST_FAR;yoffset++) {
-				for(int zoffset=-DIST_FAR;zoffset<=DIST_FAR;zoffset++) {
-					int bx = x+xoffset;
-					int by = y+yoffset;
-					int bz = z+zoffset;
-					Block b = w.getBlockAt(bx,by,bz); 
-					if(b.getType()==m) {
-						double dist = Math.sqrt(xoffset*xoffset+yoffset*yoffset+zoffset*zoffset);
-						ctfar++;
-						if(dist<DIST_MID)ctmid++;
-						if(dist<DIST_NEAR)ctnear++;
-
-						// in view?
-						if(dist<DIST_MID) {
-							Vector v = new Vector(xoffset,yoffset,zoffset);
-							double ang = v.angle(lineOfSight);
-							p.sendMessage("Angle "+Double.toString(ang));
-							if(v.angle(lineOfSight)<ANGLE)
-								ctsight++;
-							if(dist<DIST_NEAR) {
-								ctsightnear++;
-							}
-						}
-					}
-				}
-			}
-		}
-		p.sendMessage(Integer.toString(ctfar)+" blocks found within "+Integer.toString(DIST_FAR));
-		p.sendMessage(Integer.toString(ctmid)+" blocks found within "+Integer.toString(DIST_MID)+ 
-				" of which "+Integer.toString(ctsight)+" are in front of you");
-		p.sendMessage(Integer.toString(ctnear)+" blocks found within "+Integer.toString(DIST_NEAR)+
-				" of which "+Integer.toString(ctsightnear)+" are in front of you");
-	}
+    @EventHandler
+          public void onPlayerInteract(PlayerInteractEvent e) {
+              if(e.getAction()==Action.RIGHT_CLICK_BLOCK && e.getHand().equals(EquipmentSlot.HAND)) {
+                  Player p = e.getPlayer();
+                  ItemStack st = p.getInventory().getItemInMainHand();
+                  ItemMeta meta = st.getItemMeta();
+                  List<String> lore = meta.getLore();
+                  if(lore != null && lore.size()>1) {
+                	  p.sendMessage("snark 1 size="+Integer.toString(lore.size()));
+                      if(lore.get(0).equals("Magic Stick")){
+                          Material m = Material.getMaterial(lore.get(1));
+                          scan(p,m);
+                          e.setCancelled(true);
+                      }
+                  }
+              }
+          }	
+    
+    static final int DIST_NEAR=10;
+    static final int DIST_MID=20;
+    static final int DIST_FAR=60;
+    static final double ANGLE=45 * (Math.PI/180.0);
+    
+    private void scan(Player p,Material m) {
+        Location l = p.getLocation();
+        int x = l.getBlockX();
+        int y = l.getBlockY();
+        int z = l.getBlockZ();
+        World w = l.getWorld();
+        
+        Vector lineOfSight = p.getEyeLocation().getDirection();
+        
+        int ctnear =0;
+        int ctmid = 0;
+        int ctfar = 0;
+        int ctsight=0;
+        int ctsightnear=0;
+        double closest = -1;
+        p.sendMessage("looking (insert mystical sounds here)");
+        for(int xoffset=-DIST_FAR;xoffset<=DIST_FAR;xoffset++) {
+            for(int yoffset=-DIST_FAR;yoffset<=DIST_FAR;yoffset++) {
+                for(int zoffset=-DIST_FAR;zoffset<=DIST_FAR;zoffset++) {
+                    int bx = x+xoffset;
+                    int by = y+yoffset;
+                    int bz = z+zoffset;
+                    Block b = w.getBlockAt(bx,by,bz); 
+                    if(b.getType()==m) {
+                        double dist = Math.sqrt(xoffset*xoffset+yoffset*yoffset+zoffset*zoffset);
+                        if(closest<0 || dist<closest)closest=dist;
+                        ctfar++;
+                        if(dist<DIST_MID)ctmid++;
+                        if(dist<DIST_NEAR)ctnear++;
+                        
+                        // in view?
+                        if(dist<DIST_MID) {
+                            Vector v = new Vector(xoffset,yoffset,zoffset);
+                            double ang = v.angle(lineOfSight);
+                            //							p.sendMessage("Angle "+Double.toString(ang));
+                            if(v.angle(lineOfSight)<ANGLE){
+                                ctsight++;
+                                if(dist<DIST_NEAR)
+                                    ctsightnear++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        p.sendMessage(Integer.toString(ctfar)+" blocks found within "+Integer.toString(DIST_FAR));
+        p.sendMessage(Integer.toString(ctmid)+" blocks found within "+Integer.toString(DIST_MID)+ 
+                  " of which "+Integer.toString(ctsight)+" are in front of you");
+        p.sendMessage(Integer.toString(ctnear)+" blocks found within "+Integer.toString(DIST_NEAR)+
+                  " of which "+Integer.toString(ctsightnear)+" are in front of you");
+        p.sendMessage("Closest block is "+Integer.toString((int)closest));
+    }
 }
