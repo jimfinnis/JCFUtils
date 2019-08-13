8077f9b36adb34de7ae81fba0af9affb88f95563
Jim Finnis (on Hecate) <jim.finnis@gmail.com>
2019-08-13 23:36:34 +0100

    oops, forgot the player move listener.

---
+++ JCFUtils/src/org/pale/jcfutils/listeners/PlayerMoveListener.java
@@ -0,0 +1,45 @@
+package org.pale.jcfutils.listeners;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.pale.jcfutils.region.Region;
+import org.pale.jcfutils.region.RegionManager;
+
+public class PlayerMoveListener implements Listener {
+	private static final double DISTHRESHOLD = 2;
+	
+	Map<Player,Location> locsForPlayer = new HashMap<Player,Location>();
+	Map<Player,Region> regionForPlayer = new HashMap<Player,Region>();
+	
+	@EventHandler
+	public void move(final PlayerMoveEvent evt) {
+		Player p = evt.getPlayer();
+		Location l = evt.getTo();
+		
+		if(locsForPlayer.containsKey(p)) {
+			if(locsForPlayer.get(p).distance(l)>DISTHRESHOLD) {
+				checkRegions(p);
+				locsForPlayer.put(p, l);
+			}
+		} else {
+			checkRegions(p);
+			locsForPlayer.put(p, l);
+		}
+	}
+	
+	private void checkRegions(Player p) {
+		Region r = RegionManager.getManager(p.getWorld()).getSmallestRegion(p.getLocation());
+		if(r!=null && r!=regionForPlayer.get(p)) {
+			// region has changed
+			regionForPlayer.put(p, r);
+			// so display a funky title
+			p.sendTitle(r.name, "",10,70,20);
+		}
+	}
+}
